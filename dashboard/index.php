6c73585c5f183feef26774a30ae1ac8b4c0a7a3c
nadah79 <nadah.hesham.sh@gmail.com>
2023-03-20 14:46:36 +0200

    add dashboard login

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,8 +1,42 @@
 <?php
 
 include('config.php');
-echo $_POST["fname"]
-isset($_POST['Submit'])
+// echo $_POST["fname"];
+isset($_POST['Submit']);
 
 ?>
 
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+
+
+    <!-- <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div> -->
+
+    <form action="" method="POST">
+            <img src="../imgages/logo.svg" alt="">
+        <div class="form-group">
+            <label>Email </label>
+            <input type="email" name="email" class="form-control" placeholder="enter your email">
+        </div>
+
+        <div class="form-group">
+            <label>Password </label>
+            <input type="password" name="password" class="form-control" placeholder="enter your password">
+        </div>
+        <div class="form-group">
+            <button type="submit" name="submit" class="btn btn-outline-light btn-lg px-5">login</button>
+        </div>
+    </form>
+
+</body>
